e9e7d0609b0eddd70ba5862932b1ab7204337229
Yoshihiro <y.takahara@gmail.com>
2014-07-13 16:53:43 +0900

    Minerへの報酬

--- src/main.cpp
+++ src/main.cpp
@@ -1090,8 +1090,8 @@ int64 static GetBlockValue(int nHeight, int64 nFees)
 {
     int64 nSubsidy = 50 * COIN;
 
-    // Subsidy is cut in half every 840000 blocks, which will occur approximately every 4 years
-    nSubsidy >>= (nHeight / 840000); // Litecoin: 840k blocks in ~4 years
+    // Subsidy is cut in half every 1051200 blocks, which will occur approximately every 3 years
+    nSubsidy >>= (nHeight / 1051200); // Monacoin: 1051k blocks in ~3 years
 
     return nSubsidy + nFees;
 }
