520de48ab40c52318e67dc8d69869c84b96d864c
Yoshihiro <y.takahara@gmail.com>
2014-07-13 15:43:29 +0900

    remove DNS Seeder

--- src/net.cpp
+++ src/net.cpp
@@ -1192,18 +1192,12 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {"litecointools.com", "dnsseed.litecointools.com"},
-    {"litecoinpool.org", "dnsseed.litecoinpool.org"},
-    {"xurious.com", "dnsseed.ltc.xurious.com"},
-    {"koin-project.com", "dnsseed.koin-project.com"},
-    {"weminemnc.com", "dnsseed.weminemnc.com"},
+//    {"monacoin.org", "dnsseed.monacoin.org"},
     {NULL, NULL}
 };
 
 static const char *strTestNetDNSSeed[][2] = {
-    {"litecointools.com", "testnet-seed.litecointools.com"},
-    {"xurious.com", "testnet-seed.ltc.xurious.com"},
-    {"wemine-testnet.com", "dnsseed.wemine-testnet.com"},
+//    {"monacoin.org", "test-dnsseed.monacoin.org"},
     {NULL, NULL}
 };
 
